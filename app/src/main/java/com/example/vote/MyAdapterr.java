5d9d85ff35888e8396fb8f71d87feafcf0e6df60
Mohamed Beirouk <mbeirouck@gmail.com>
2022-07-01 13:55:48 +0000

    BarCahrt fin

--- app/src/main/java/com/example/vote/MyAdapterr.java
+++ app/src/main/java/com/example/vote/MyAdapterr.java
@@ -8,10 +8,17 @@ import android.widget.CheckBox;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.biometric.BiometricPrompt;
-import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -19,13 +26,14 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.concurrent.Executor;
 
 
 public class MyAdapterr extends RecyclerView.Adapter<MyAdapterr.MyViewHolder> {
     Context context;
     ArrayList<votes> listvotes;
     ArrayList<votesr> votesrlist = new ArrayList<>();
+    ArrayList barArrayList;
+
 
     public MyAdapterr(Context context, ArrayList<com.example.vote.votes> votes) {
         this.context = context;
@@ -42,6 +50,7 @@ public class MyAdapterr extends RecyclerView.Adapter<MyAdapterr.MyViewHolder> {
             }@Override
             public void onCancelled(@NonNull DatabaseError error) {}
         });
+        //inidata();
     }
 
     @NonNull
@@ -51,6 +60,7 @@ public class MyAdapterr extends RecyclerView.Adapter<MyAdapterr.MyViewHolder> {
 
         return new MyViewHolder(v);
     }
+
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
@@ -78,24 +88,48 @@ public class MyAdapterr extends RecyclerView.Adapter<MyAdapterr.MyViewHolder> {
 
         }
         if(vote.getRe3().length()<1){
-            holder.re3.setVisibility(View.GONE);
-            Log.e(" cond", "re3 = "+vote.getRe3());
+//            holder.re3.setVisibility(View.GONE);
+//            Log.e(" cond", "re3 = "+vote.getRe3());
         }
         if(vote.getRe4().length()<1){
-            holder.re4.setVisibility(View.GONE);
+//            holder.re4.setVisibility(View.GONE);
         }
         if(vote.getRe5().length()<1){
-            holder.re5.setVisibility(View.GONE);
+//            holder.re5.setVisibility(View.GONE);
         }
 
-        holder.re1.setText(vote.getRe1()+" a eu "+nr1+" votes");
-        holder.re2.setText(vote.getRe2()+" a eu "+nr2+" votes");
-        holder.re3.setText(vote.getRe3()+" a eu "+nr3+" votes");
-        holder.re4.setText(vote.getRe4()+" a eu "+nr4+" votes");
-        holder.re5.setText(vote.getRe5()+" a eu "+nr5+" votes");
+//        holder.re1.setText(vote.getRe1()+" a eu "+nr1+" votes");
+//        holder.re2.setText(vote.getRe2()+" a eu "+nr2+" votes");
+//        holder.re3.setText(vote.getRe3()+" a eu "+nr3+" votes");
+//        holder.re4.setText(vote.getRe4()+" a eu "+nr4+" votes");
+//        holder.re5.setText(vote.getRe5()+" a eu "+nr5+" votes");
+
+        inidata();
+        BarDataSet bardataset;
+        bardataset = new BarDataSet(barArrayList, "question");
+        BarData bardata = new BarData(bardataset);
+        holder.barchart.setData(bardata);
+        bardataset.setColors(ColorTemplate.COLORFUL_COLORS);
+        bardataset.setValueTextColor(android.R.color.black);
+        bardataset.setValueTextSize(16f);
+        holder.barchart.getDescription().setEnabled(true);
+        XAxis xAxis = holder.barchart.getXAxis();
+        final String[] labels = new String[] {"Dummy", "Jan", "Feb", "March", "April","mai","jun"};
+        xAxis.setValueFormatter(new IndexAxisValueFormatter(labels));
+        xAxis.setGranularity(0f);
+        xAxis.setGranularityEnabled(true);
 
 
 
+    }
+    public void inidata()
+    {
+        barArrayList = new ArrayList();
+        barArrayList.add(new BarEntry(2f,10));
+        barArrayList.add(new BarEntry(3f,20));
+        barArrayList.add(new BarEntry(4f,30));
+        barArrayList.add(new BarEntry(5f,40));
+        barArrayList.add(new BarEntry(6f,50));
     }
         @Override
         public int getItemCount () {
@@ -105,16 +139,25 @@ public class MyAdapterr extends RecyclerView.Adapter<MyAdapterr.MyViewHolder> {
         public static class MyViewHolder extends RecyclerView.ViewHolder {
             TextView question;
             CheckBox re1, re2, re3, re4, re5;
-
+            BarChart barchart;
             public MyViewHolder(@NonNull View itemView) {
                 super(itemView);
                 question = itemView.findViewById(R.id.textquestionn);
-                re1 = itemView.findViewById(R.id.r1checkn);
-                re2 = itemView.findViewById(R.id.r2checkn);
-                re3 = itemView.findViewById(R.id.r3checkn);
-                re4 = itemView.findViewById(R.id.r4checkn);
-                re5 = itemView.findViewById(R.id.r5checkn);
+                barchart = itemView.findViewById(R.id.barchart);
+
+//                re1 = itemView.findViewById(R.id.r1checkn);
+//                re2 = itemView.findViewById(R.id.r2checkn);
+//                re3 = itemView.findViewById(R.id.r3checkn);
+//                re4 = itemView.findViewById(R.id.r4checkn);
+//                re5 = itemView.findViewById(R.id.r5checkn);
+
+
+
+
 
             }
+
         }
+
+
     }
